81909646882cd0c875a6f68b2f6cad7b8e3e1051
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2024-12-18 23:24:14 +0600

    [done] userAgent data view issue solved

--- src/app/user-agent/page.tsx
+++ src/app/user-agent/page.tsx
@@ -1,7 +1,18 @@
-import { UserAgent } from "@/views/userAgent";
+import React from "react";
+import { headers } from "next/headers";
+import { UserAgentProvider } from "@/components/providers/userAgentProvider";
+import { UserAgent } from "@/views/userAgent/userAgent";
 
 const UserAgentRoot = () => {
-  return <UserAgent />;
+  const headersList = headers();
+  const userAgent = headersList.get("user-agent") || "Unknown User-Agent";
+
+  return (
+    <UserAgentProvider userAgent={userAgent}>
+      <UserAgent />
+    </UserAgentProvider>
+  );
 };
 
 export default UserAgentRoot;
+
